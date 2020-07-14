dde78f41dc4d0108c75f44a14eaeaa7e2f155a40
Salvatore Cozzubo <salvatore.cozzubo@ctmobi.it>
2020-07-14 16:28:02 +0200

    rimossa uuid

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 const AddTodoForm = ({ dispatch, createTodo }) => {
   const [formState, setFormState] = useState('');
